a1eb1dacf3664d239715df42191609b5eafba16a
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2025-05-10 20:27:20 -0300

    v3.1

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,12 @@ export default function App() {
         'dots',
         'circle',
         'fluid',
+        'gravity',
         'reverse',
         'shower',
-        'gravity',
-        'morse'
+        'morse',
+        'italic',
+        'inclined'
     ];
 
     const colorEffect : Array<QrcodeColorEffect> = [
